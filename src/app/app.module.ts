fa6ca34aefd4286ebd41223b894b53696b10c00f
Rodrigo Gouveia <rgouveia@trt13.jus.br>
2018-11-26 21:36:58 -0300

    Add angular firestore  configuration- Set access data to the firestore service in environment file- Add AngularFirestoreModule into the app.modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { BrowserModule } from '@angular/platform-browser';
 
+import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +13,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
