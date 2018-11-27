5e290a360a0b083ab85716df372c3b571aba144c
Rodrigo Gouveia <rgouveia@trt13.jus.br>
2018-11-27 18:47:17 -0300

    Fix the unit tests of app.component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,18 @@
 import { async, TestBed } from '@angular/core/testing';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
 import { RouterTestingModule } from '@angular/router/testing';
+
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        AngularFirestoreModule,
+        AngularFireModule.initializeApp(environment.firebase),
+        RouterTestingModule,
       ],
       declarations: [
         AppComponent
